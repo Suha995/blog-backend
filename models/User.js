defb635af755d28ecfe8ab7c2da929fa1d2bf855
Suhair <saljammal995@gmail.com>
2023-08-02 22:02:26 +0300

    Credentials validation and JWT

--- models/User.js
+++ models/User.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
-
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+//we can build here custom validation
 const UserSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     email: {
       type: String,
-      required: true,
-      unique: true,
+      unique: true, //we can't use custom validation for this error
+      required: [true, "Please enter an email"], //custom validation
+      validate: [isEmail, "Please enter a valid email"],
+      lowercase: true,
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Minimun password length is 6"], //this doesn't work
     },
     profilePic: {
       type: String,
@@ -24,4 +24,22 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+//Mongoose Hooks
+//fire a function after doc saved to the db
+// "save": the type of event
+// UserSchema.post("save", (doc, next) => {
+//   console.log("a new user was craeted and saved", doc);
+//   next(); // to get to the next middleware in the stack
+//   //if we dont use it the res will not be sended to the client
+// });
+
+//fire a function before doc is saved to the db
+UserSchema.pre("save", async function (next) {
+  //this //refers to the local instance of the user
+  //we dont use arrow function in order to have access to this
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model("user", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,42 +1,80 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+
+  //email is not unique//duplicate error code
+  if (err.code === 11000) {
+    errors.email = "this email is already registered";
+    return errors;
+  }
+
+  //validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message; //updating errors object.
+    });
+  }
+  return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  //sign takes payload object/secret key/options
+  //header is automatically created
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: maxAge,
+    //in seconds not in ms.
+  });
+};
 
 //Register
 router.post("/register", async (req, res) => {
+  const { password, email } = req.body;
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const user = new User({
-      username: req.body.username,
-      password: hashedPassword,
-      email: req.body.email,
+    //Mongoose Pre Hook is fired here //1:validate credentials and hash password
+    //2: save new user doc in db
+    const user = await User.create({ email, password });
+    //3: create JWT token
+    const token = createToken(user._id);
+
+    //4: set cookie
+    res.cookie("jwt", token, {
+      maxAge: maxAge * 1000,
+      httpOnly: true,
     });
 
-    const result = await user.save(); //return the new user
-    res.status(200).json(result);
+    res.status(201).json({ user });
   } catch (err) {
-    res.status(500).json(err);
+    console.log(err.message);
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 });
 
 //Login
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
+    const user = await User.findOne({ email: req.body.email });
     // !user && res.status(400).json("Wrong credentials");
+    const errors = { email: "", password: "" };
     if (!user) {
-      res.status(400).json("Wrong credentials");
+      errors.email = "The email is not correct";
+      res.status(400).json({ errors });
       return;
     }
-
     const validated = await bcrypt.compare(req.body.password, user.password);
     // !validated && res.status(400).json("Wrong credentials");
     if (!validated) {
-      res.status(400).json("Wrong credentials");
+      errors.password = "The password is not correct";
+      res.status(400).json({ errors });
       return;
     }
-
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { maxAge: maxAge * 1000 });
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
