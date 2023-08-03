d71b1ec9818d0e3818cb6983d015e4fc1c4b5b8f
Suhair <saljammal995@gmail.com>
2023-08-03 14:17:44 +0300

    solve the set cookie problem

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const path = require("path");
 
 dotenv.config();
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
